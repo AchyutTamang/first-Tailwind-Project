920c8cd2f7c56f9c0e01aad9af904b727f56f194
AchyutTamang <achyuttamang23@gmail.com>
2025-02-11 20:19:59 +0545

    Project SetUp

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -1,17 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 class="text-3xl font-bold ">Hello world!</h1>
-      <h2 className="text-lg font-semi-bold"> lunds MUJI</h2>
-    </>
-  );
+export default function App() {
+  return <h1 class="text-centre text-[50px] underline">Hello world!</h1>;
 }
-
-export default App;
